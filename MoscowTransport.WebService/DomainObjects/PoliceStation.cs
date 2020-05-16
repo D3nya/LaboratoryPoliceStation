1c93ed0e5de1bf86eadb7db944d3a01c95a60983
Денис Мясников <myasnicov2011@yandex.ru>
2020-05-16 06:59:40 +0300

    Laboratory5_version4

--- MoscowTransport.WebService/DomainObjects/PoliceStation.cs
+++ MoscowTransport.WebService/DomainObjects/PoliceStation.cs
@@ -10,6 +10,6 @@ namespace PoliceStations.DomainObjects
         public string AdmArea { get; set; }
         public string District { get; set; }
         public string Address { get; set; }
-        public string ExtraInfo { get; set; }
+        public string ExtraInfo { get; set; }   
     }
 }
---
+++ MoscowTransport.WebService/Migrations/20200516035718_Initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PoliceStations.WebService.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PoliceStations",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    AdmArea = table.Column<string>(nullable: true),
+                    District = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    ExtraInfo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PoliceStations", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "PoliceStations",
+                columns: new[] { "Id", "Address", "AdmArea", "District", "ExtraInfo", "Name" },
+                values: new object[] { 1L, "Шубинский переулок, дом 7", "Центральный административный округ", "район Арбат", "ФИО участкового, часы приема можно узнать через специальную форму поиска на сайте petrovka38.ru/Kontaktn…", "Участковый пункт полиции № 1 по району Арбат" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PoliceStations");
+        }
+    }
+}
