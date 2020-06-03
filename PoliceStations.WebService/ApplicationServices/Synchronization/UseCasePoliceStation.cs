7462f61b40781397f89ee69646d55d3ad66fc60d
Денис Мясников <myasnicov2011@yandex.ru>
2020-06-04 00:20:42 +0300

    Laboratory7

--- PoliceStations.WebService/ApplicationServices/Synchronization/UseCasePoliceStation.cs
+++ PoliceStations.WebService/ApplicationServices/Synchronization/UseCasePoliceStation.cs
@@ -5,6 +5,11 @@ using PoliceStations.DomainObjects;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using PoliceStations.InfrastructureServices.Gateways.Database;
 
 namespace PoliceStations.ApplicationServices.Synchronization
 {
@@ -23,10 +28,11 @@ namespace PoliceStations.ApplicationServices.Synchronization
         public string ExtraInfo { get; set; }
     }
 
-
     public class UseCasePoliceStation
     {
         static string remoteURL = "https://apidata.mos.ru/v1/datasets/2354/rows?api_key=a81609408dea292d2b1278d155511711";
+
+        string path = @".\update_database\PoliceStation-";
 
         List<PoliceStation_Cell> policeStation_cells;
 
@@ -38,7 +44,6 @@ namespace PoliceStations.ApplicationServices.Synchronization
             WebRequest request = WebRequest.Create(remoteURL);
             WebResponse response = request.GetResponse();
 
-
             StreamReader stream = new StreamReader(response.GetResponseStream());
             string line = stream.ReadToEnd();
 
@@ -46,19 +51,50 @@ namespace PoliceStations.ApplicationServices.Synchronization
 
             policeStation_cells = JsonConvert.DeserializeObject<List<PoliceStation_Cell>>(jsonArray.ToString());
 
+            DateTime Date_update = DateTime.Now;
+            string date_update = Date_update.ToShortDateString();
 
-            for (int i = 0; i < policeStation_cells.Count; i++)
+            path = path + date_update + @".json";
+
+            using (FileStream fs2 = new FileStream(path, FileMode.OpenOrCreate))
             {
-                policeStations.Add(new PoliceStation()
+
+                var options = new JsonSerializerOptions
                 {
-                    Name = policeStation_cells[i].Cells.Name,
-                    Id = policeStation_cells[i].Number,
-                    AdmArea = policeStation_cells[i].Cells.AdmArea,
-                    Address = policeStation_cells[i].Cells.Address,
-                    ExtraInfo = policeStation_cells[i].Cells.ExtraInfo
-                });
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                    WriteIndented = true
+                };
+
+                string text = "[";
+                byte[] input = Encoding.Default.GetBytes(text);
+
+                fs2.Write(input, 0, input.Length);
+                text = ",";
+
+                for (int i = 0; i < policeStation_cells.Count; i++)
+                {
+                    policeStations.Add(new PoliceStation()
+                    {
+                        Name = policeStation_cells[i].Cells.Name,
+                        Id = policeStation_cells[i].Number,
+                        AdmArea = policeStation_cells[i].Cells.AdmArea,
+                        Address = policeStation_cells[i].Cells.Address,
+                        ExtraInfo = policeStation_cells[i].Cells.ExtraInfo
+                    });
+
+                    System.Text.Json.JsonSerializer.SerializeAsync<PoliceStation>(fs2, policeStations[i], options).GetAwaiter().GetResult();
+
+                    if (i != policeStation_cells.Count - 1)
+                    {
+                        input = Encoding.Default.GetBytes(text);
+                        fs2.Write(input, 0, input.Length);
+                    }
+                }
+
+                text = "]";
+                input = Encoding.Default.GetBytes(text);
+                fs2.Write(input, 0, input.Length);
             }
         }
-
     }
 }
